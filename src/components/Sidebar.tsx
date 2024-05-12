561b6ff491177222e8c94a6987b0d7893c36b126
Mask512 <mask.roktz@gmail.com>
2024-05-13 02:02:50 +0700

    sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,17 @@
-import { Box } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from '@mui/material';
+import { DarkMode, Group, Groups, Home, Pages, Person2, Settings, ShoppingBasket,  } from '@mui/icons-material';
 
 export const Sidebar = () => {
   return (
     <Box
-      bgcolor="skyblue"
       flex={1}
       p={2}
       sx={{
@@ -13,7 +21,59 @@ export const Sidebar = () => {
         },
       }}
     >
-      Sidebar
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#home'>
+              <ListItemIcon><Home /></ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#pages'>
+              <ListItemIcon><Pages /></ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#groups'>
+              <ListItemIcon><Group /></ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#marketplace'>
+              <ListItemIcon><ShoppingBasket /></ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#friends'>
+              <ListItemIcon><Groups/></ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#settings'>
+              <ListItemIcon><Settings /></ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#profile'>
+              <ListItemIcon><Person2 /></ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon><DarkMode /></ListItemIcon>
+            <Switch/>
+            </ListItemButton>
+          </ListItem>
+          
+        </List>
+      </nav>
     </Box>
   );
 };
